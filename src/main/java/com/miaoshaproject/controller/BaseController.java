9c336c2a201a247a8542a2788a9f44dfab7e3853
god-jiang <1440113361@qq.com>
2019-12-24 23:06:51 +0800

    更新otp验证码

--- src/main/java/com/miaoshaproject/controller/BaseController.java
+++ src/main/java/com/miaoshaproject/controller/BaseController.java
@@ -17,21 +17,26 @@ import java.util.Map;
  * @date 2019/12/22
  */
 public class BaseController {
+    /**
+     * 后端要消费的前端的数据类型
+     */
+    public static final String CONTENT_TYPE_FORMED = "application/x-www-form-urlencoded";
+
     //定义exceptionhandler解决未被controller层吸收的exception
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public Object handlerException(HttpServletRequest request, Exception ex){
-        Map<String,Object> responseData=new HashMap<>();
-        CommonReturnType commonReturnType=new CommonReturnType();
-        if(ex instanceof BusinessException){
-            BusinessException businessException=(BusinessException)ex;
-            responseData.put("errCode",businessException.getErrCode());
-            responseData.put("errMsg",businessException.getErrMsg());
-        }else{
+    public Object handlerException(HttpServletRequest request, Exception ex) {
+        Map<String, Object> responseData = new HashMap<>();
+        CommonReturnType commonReturnType = new CommonReturnType();
+        if (ex instanceof BusinessException) {
+            BusinessException businessException = (BusinessException) ex;
+            responseData.put("errCode", businessException.getErrCode());
+            responseData.put("errMsg", businessException.getErrMsg());
+        } else {
             responseData.put("errCode", EmBusinessError.UNKNOWN_ERROR.getErrCode());
-            responseData.put("errMsg",EmBusinessError.UNKNOWN_ERROR.getErrMsg());
+            responseData.put("errMsg", EmBusinessError.UNKNOWN_ERROR.getErrMsg());
         }
-        return commonReturnType.create(responseData,"fail");
+        return commonReturnType.create(responseData, "fail");
     }
 }
--- src/main/java/com/miaoshaproject/controller/UserController.java
+++ src/main/java/com/miaoshaproject/controller/UserController.java
@@ -11,13 +11,39 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Random;
+
 
 @Controller
 @RequestMapping("/user")
-public class UserController extends BaseController{
+@CrossOrigin(allowCredentials = "true",allowedHeaders = "*")
+public class UserController extends BaseController {
 
     @Autowired
     private UserService userService;
+
+    @Autowired
+    private HttpServletRequest httpServletRequest;//通过他绑定otp和手机号码
+
+    //用户获取otp短信接口
+    @RequestMapping(value = "/getotp", method = {RequestMethod.POST},consumes = CONTENT_TYPE_FORMED)
+    @ResponseBody
+    public CommonReturnType getOtp(@RequestParam(name = "telphone") String telphone) {
+        //需要按照一定的规则生成OTP验证码
+        Random random = new Random();
+        //生成一个[0,99999)的数
+        int randomInt = random.nextInt(99999);
+        randomInt += 10000;
+        String otpCode = String.valueOf(randomInt);
+
+        //将OTP验证码同对应用户的手机号关联，使用httpsession的方式绑定他的手机号与OTPCODE
+        httpServletRequest.getSession().setAttribute(telphone, otpCode);
+
+        //将OTP验证码通过短信通道发送给用户，省略
+        System.out.println("telphone =" + telphone + "&otpCode=" + otpCode);
+        return CommonReturnType.create(null);
+    }
 
     @RequestMapping("/get")
     @ResponseBody
@@ -25,7 +51,7 @@ public class UserController extends BaseController{
         //调用service服务获取对应id的用户对象并返回给前端
         UserModel userModel = userService.getUserById(id);
         //若获取的对应用户信息不存在
-        if(userModel==null){
+        if (userModel == null) {
             throw new BusinessException(EmBusinessError.USER_NOT_EXIST);
         }
         //将核心领域模型用户对象转换为可供UI使用的viewobject
@@ -35,14 +61,13 @@ public class UserController extends BaseController{
         return returnType.create(userVO);
     }
 
-    private UserVO convertFromModel(UserModel userModel){
-        if(userModel==null){
+    private UserVO convertFromModel(UserModel userModel) {
+        if (userModel == null) {
             return null;
         }
-        UserVO userVO=new UserVO();
-        BeanUtils.copyProperties(userModel,userVO);
+        UserVO userVO = new UserVO();
+        BeanUtils.copyProperties(userModel, userVO);
         return userVO;
-
     }
 
 }
